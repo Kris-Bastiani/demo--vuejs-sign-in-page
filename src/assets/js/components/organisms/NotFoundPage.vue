9e91313d89ec8c5c08995733258fac67fedad01e
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-23 17:26:18 +1000

    Create NotFoundPage.vue

---
+++ src/assets/js/components/organisms/NotFoundPage.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+	app-page
+		.container.dsp__flex.pad__sml.w_max__reg
+			h1 Page not found
+</template>
+
+<script>
+import AppPage from '../molecules/AppPage.vue';
+
+export default {
+	components: { AppPage },
+}
+</script>
