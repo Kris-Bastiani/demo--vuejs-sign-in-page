9645037344bda8e4239288d877f10dce08fa9ab4
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-23 21:53:15 +1000

    Set up InputField component

---
+++ src/assets/js/components/atoms/InputField.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+	label.input_field
+		span.input_field__label {{ label }}
+		input(:placeholder='placeholder', :type='type')
+</template>
+
+<script>
+export default {
+	props: {
+		label: String,
+		placeholder: String,
+		type: String,
+	},
+};
+</script>
--- src/assets/js/components/molecules/LoginForm.vue
+++ src/assets/js/components/molecules/LoginForm.vue
@@ -1,4 +1,18 @@
 <template lang="pug">
 	form
+		input-field(label='Email:', placeholder='Enter email', type='email')
+
+		input-field(label='Password:', placeholder='Enter password', type='password')
+
 		a(href='/new-password.pug') Want to reset your password?
+
+		input(type='submit', value='Login')
 </template>
+
+<script>
+import InputField from '../atoms/InputField.vue';
+
+export default {
+	components: { InputField },
+};
+</script>
