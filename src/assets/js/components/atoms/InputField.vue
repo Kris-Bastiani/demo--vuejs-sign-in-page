16f8159d864b4187d2e9978d525f67085b6a49f6
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-26 08:21:53 +1000

    Bind blur and focus events in InputField

--- src/assets/js/components/atoms/InputField.vue
+++ src/assets/js/components/atoms/InputField.vue
@@ -1,12 +1,23 @@
 <template lang="pug">
 	label.input_field(:class='{ _invalid: error }')
 		span.input_field__label {{ label }}
-		input(:placeholder='placeholder', :type='type', :value='value', @input="$emit('input', $event.target.value)")
+		input(:placeholder='placeholder', :type='type', :value='value', v-on='inputListeners')
 		.input_field__error(v-if='error') {{ error }}
 </template>
 
 <script>
 export default {
+	computed: {
+		inputListeners() {
+			const vm = this;
+			return {
+				...this.$listeners,
+				blur() { vm.$emit('blur'); },
+				focus() { vm.$emit('focus'); },
+				input(e) { vm.$emit('input', e.target.value); },
+			};
+		},
+	},
 	props: {
 		error: String,
 		label: String,
