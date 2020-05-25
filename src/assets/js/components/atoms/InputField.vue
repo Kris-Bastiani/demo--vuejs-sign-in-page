48852e610a5c17e708052438d1c2eb11211abfea
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-25 15:23:45 +1000

    Add error message to InputField

--- src/assets/js/components/atoms/InputField.vue
+++ src/assets/js/components/atoms/InputField.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
-	label.input_field
+	label.input_field(:class='{ _invalid: error }')
 		span.input_field__label {{ label }}
 		input(:placeholder='placeholder', :type='type')
+		.input_field__error(v-if='error') {{ error }}
 </template>
 
 <script>
 export default {
 	props: {
+		error: String,
 		label: String,
 		placeholder: String,
 		type: String,
