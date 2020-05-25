44c7e0fdfa579cf2fdefc8a4e8d1853497436d14
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-25 14:19:48 +1000

    Add dropdown menu to UserMenu

--- src/assets/js/components/atoms/UserMenu.vue
+++ src/assets/js/components/atoms/UserMenu.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
-	button.user_menu
+	button.user_menu(@click='toggleIsOpen()' :class='{ _open: isOpen }')
 		img.avatar(src='/assets/images/avatar.png', alt='User Menu')
 		app-icon(type='chevron', role='presentation')
+		ul.user_menu__options
+			li: button(@click='signInOrOut()') Sign {{ currentUser ? 'out' : 'in' }}
 </template>
 
 <script>
@@ -9,5 +11,19 @@ import AppIcon from './AppIcon.vue';
 
 export default {
 	components: { AppIcon },
+	data: () => ({
+		currentUser: null,
+		isOpen: false,
+	}),
+	methods: {
+		signInOrOut() {
+			if (!this.currentUser) {
+				window.location = '/';
+			} else {
+				console.log('Signing out...');
+			}
+		},
+		toggleIsOpen() { this.isOpen = !this.isOpen; },
+	},
 };
 </script>
