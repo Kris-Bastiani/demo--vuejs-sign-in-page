c5339b196e4aa3f95604d5a5d5d3fcafa25b3b20
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-25 17:24:54 +1000

    Add login form submission

--- src/assets/js/components/molecules/LoginForm.vue
+++ src/assets/js/components/molecules/LoginForm.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
-	form(title='login form')
-		input-field(label='Email:', placeholder='Enter email', type='email')
+	form(title='login form', @submit='e => signIn(e)')
+		input-field(label='Email:', placeholder='Enter email', type='email' v-model='email')
 
-		input-field(label='Password:', placeholder='Enter password', type='password')
+		input-field(label='Password:', placeholder='Enter password', type='password' v-model='password')
 
 		a.dsp__block.mrg_b__lrg(href='/new-password.pug') Want to reset your password?
 
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+import postData from '../../helpers/postData';
 import InputField from '../atoms/InputField.vue';
 import SubmitButton from './SubmitButton.vue';
 
@@ -17,6 +18,21 @@ export default {
 	components: {
 		InputField,
 		SubmitButton,
+	},
+	data: () => ({
+		email: '',
+		password: '',
+	}),
+	methods: {
+		signIn(e) {
+			e.preventDefault();
+
+			const { email, password } = this;
+
+			postData('https://reqres.in/api/login', { email, password })
+				.then(data => console.log('Success:', data))
+				.catch(console.error);
+		},
 	},
 };
 </script>
