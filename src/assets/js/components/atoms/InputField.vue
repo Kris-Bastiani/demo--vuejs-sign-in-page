f14e4e2e86f3d0f2ea3f50746c1e0da985ad0d23
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-25 17:17:36 +1000

    Allow v-model binding for InputField

--- src/assets/js/components/atoms/InputField.vue
+++ src/assets/js/components/atoms/InputField.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 	label.input_field(:class='{ _invalid: error }')
 		span.input_field__label {{ label }}
-		input(:placeholder='placeholder', :type='type')
+		input(:placeholder='placeholder', :type='type', :value='value', @input="$emit('input', $event.target.value)")
 		.input_field__error(v-if='error') {{ error }}
 </template>
 
@@ -12,6 +12,7 @@ export default {
 		label: String,
 		placeholder: String,
 		type: String,
+		value: String,
 	},
 };
 </script>
