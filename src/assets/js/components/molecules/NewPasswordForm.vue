faf733c4016754b4de8c4c0f540fc52eb4c46837
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-26 14:00:03 +1000

    Add validation to NewPasswordForm

--- src/assets/js/components/molecules/NewPasswordForm.vue
+++ src/assets/js/components/molecules/NewPasswordForm.vue
@@ -1,13 +1,30 @@
 <template lang="pug">
 	form(title='new password form')
-		input-field(label='New password:', placeholder='Enter new password', type='password', required)
+		input-field(
+			label='New password:',
+			placeholder='Enter new password',
+			type='password',
+			required,
+			v-model='password.value',
+			@blur="e => onInputBlur(e, 'password')",
+			:error='password.error',
+		)
 
-		input-field.mrg_b__lrg(label='Confirm password:', placeholder='Confirm new password', type='password', required)
+		input-field.mrg_b__lrg(
+			label='Confirm password:',
+			placeholder='Confirm new password',
+			type='password',
+			required,
+			v-model='confirmPassword.value',
+			@blur="e => onInputBlur(e, 'confirmPassword')",
+			:error='confirmPassword.error',
+		)
 
-		submit-button(label='Save')
+		submit-button(label='Save', :class='{ _in_progress: inProgress }', :disabled='canSubmit')
 </template>
 
 <script>
+import validateInput from '../../helpers/validateInput';
 import InputField from '../atoms/InputField.vue';
 import SubmitButton from './SubmitButton.vue';
 
@@ -15,6 +32,29 @@ export default {
 	components: {
 		InputField,
 		SubmitButton,
+	},
+	computed: {
+		canSubmit() { return !(this.password.value.length && this.confirmPassword.value.length); },
+	},
+	data: () => ({
+		password: {
+			error: null,
+			value: '',
+		},
+		confirmPassword: {
+			error: null,
+			value: '',
+		},
+		inProgress: false,
+	}),
+	methods: {
+		onInputBlur(e, field) {
+			// eslint-disable-next-line security/detect-object-injection
+			this[field].error = validateInput(e.target);
+			if (field === 'confirmPassword' && this.password.value !== this.confirmPassword.value) {
+				this.confirmPassword.error = 'These fields need to match.';
+			}
+		},
 	},
 };
 </script>
