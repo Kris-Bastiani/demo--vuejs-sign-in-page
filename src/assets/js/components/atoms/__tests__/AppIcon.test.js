b99b968a05685734943bc2dd610e31b394be2cd6
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-27 11:07:38 +1000

    AppIcon: fix test descriptions

--- src/assets/js/components/atoms/__tests__/AppIcon.test.js
+++ src/assets/js/components/atoms/__tests__/AppIcon.test.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils';
 import AppIcon, { ICON_TYPES } from '../AppIcon.vue';
 
 describe('AppIcon', () => {
-	ICON_TYPES.forEach(type => it('Should match snapshot when type = ', () => {
+	ICON_TYPES.forEach(type => it(`Should match snapshot when type = ${type}`, () => {
 		const wrapper = mount(AppIcon, { propsData: { type } });
 		expect(wrapper.element).toMatchSnapshot();
 	}));
