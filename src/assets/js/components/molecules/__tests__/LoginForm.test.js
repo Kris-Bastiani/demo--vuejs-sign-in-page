d51f47085330df8ab428c9080d44c08bd430a38c
Kris Bastiani <kris.bastiani@gmail.com>
2020-05-27 16:17:38 +1000

    LoginForm: add tests

--- src/assets/js/components/molecules/__tests__/LoginForm.test.js
+++ src/assets/js/components/molecules/__tests__/LoginForm.test.js
@@ -5,7 +5,31 @@ import LoginForm from '../LoginForm.vue';
 describe('LoginForm', () => {
 	let wrapper;
 
-	beforeAll(() => { wrapper = shallowMount(LoginForm); });
+	beforeAll(() => {
+		wrapper = shallowMount(LoginForm);
+
+		Object.defineProperty(window, 'localStorage', {
+			value: { setItem: jest.fn(() => null) },
+			writable: true,
+		});
+	});
 
 	it('Matches snapshot', () => { expect(wrapper.element).toMatchSnapshot(); });
+
+	it('Sets appropriate input errors on input blur', () => {
+		wrapper.vm.onInputBlur({
+			target: {
+				required: true,
+				type: 'email',
+				value: null,
+			},
+		}, 'email');
+		expect(wrapper.vm.$data.email.error).toEqual('This field is required.');
+	});
+
+	it('Sets user in localStorage and sets signedIn to true on signIn', () => {
+		wrapper.vm.signIn({});
+		expect(window.localStorage.setItem).toHaveBeenCalledWith('user', '{}');
+		expect(wrapper.vm.$data.signedIn).toEqual(true);
+	});
 });
